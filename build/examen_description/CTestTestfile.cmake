3433ef3969b6faffb5d917ea39f2afa997aca60f
Robotics Lab User <salazardiego946@gmail.com>
2024-11-19 11:28:52 -0600

    Imagen Robot

---
+++ build/examen_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robousr/ROS2Dev/Robofriends/examen_description
+# Build directory: /home/robousr/ROS2Dev/Robofriends/build/examen_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
